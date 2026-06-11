755d6ec76075fb7ff87196397f71e2440c6db4e3
mad547 <97340914+mad547@users.noreply.github.com>
2026-06-11 14:32:15 +0600

    добавил модели type, status issue

--- source/tracker_app/models.py
+++ source/tracker_app/models.py
@@ -1,3 +1,70 @@
 from django.db import models
 
-# Create your models here.
+class Type(models.Model):
+    name = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False,
+        verbose_name='Название'
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'type'
+        verbose_name = 'Тип'
+        verbose_name_plural = 'Типы'
+
+
+class Status(models.Model):
+    name = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False,
+        verbose_name= 'Название'
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'status'
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статусы'
+
+
+class Issue(models.Model):
+    summary = models.CharField(
+        max_length=200,
+        null=False,
+        blank=False,
+        verbose_name= 'Краткое описание'
+    )
+    description = models.TextField(
+        null=False,
+        blank=False,
+        verbose_name= 'Полное описание'
+    )
+    status = models.ForeignKey(
+        Status,
+        on_delete=models.PROTECT,
+        null=False,
+        verbose_name= 'Тип'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name= 'Время создания'
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name= 'Время обновления'
+    )
+    def __str__(self):
+        return self.summary
+
+    class Meta:
+        db_table = 'issue'
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+        ordering = ['-created_at']
