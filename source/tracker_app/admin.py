a1a5bc4fe63dd3ceeaacb63638a3df2eff5d3ddd
mad547 <97340914+mad547@users.noreply.github.com>
2026-06-11 14:40:02 +0600

    зарегестрировал модели в админке

--- source/tracker_app/admin.py
+++ source/tracker_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from tracker_app.models import Type, Status, Issue
+
 
 # Register your models here.
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ['id', 'summary', 'type', 'status', 'created_at',]
+    list_filter = ['type', 'status',]
+    search_fields = ['summary']
+
+
+admin.site.register(Type)
+admin.site.register(Status)
+admin.site.register(Issue, IssueAdmin)
