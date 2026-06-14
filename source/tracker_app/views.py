48cf8399e458880191a5aa49b13052767931da73
mad547 <97340914+mad547@users.noreply.github.com>
2026-06-14 23:03:39 +0600

    переписал views на templateviev и formviev

--- source/tracker_app/views.py
+++ source/tracker_app/views.py
@@ -1,56 +1,74 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import redirect, get_object_or_404
+from django.urls import reverse_lazy, reverse
 from django.views import View
+from django.views.generic import TemplateView, FormView
+
+
 from tracker_app.models import Issue
 from tracker_app.forms import IssueForm
 
 
 # Create your views here.
-class IssueListView(View):
-    def get(self, request):
-        issues = Issue.objects.all()
-        return render(request, 'tracker_app/issue_list.html', {'issue': issues})
+class IssueListView(TemplateView):
+    template_name = 'tracker_app/issue_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['issues'] = Issue.objects.all()
+        return context
 
 
-class IssueDetailView(View):
-    def get(self, request, pk):
-        issue = get_object_or_404(Issue, pk=pk)
-        return render(request, 'tracker_app/issue_detail.html', {'issue': issue})
+class IssueDetailView(TemplateView):
+    template_name = 'tracker_app/issue_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super.get_context_data(**kwargs)
+        context['issue'] = get_object_or_404(Issue, pk=self.kwargs.get('pk'))
+        return context
 
 
-class IssueCreateView(View):
-    def get(self, request):
-        form = IssueForm()
-        return render(request, 'tracker_app/issue_create.html', {'form': form})
+class IssueCreateView(FormView):
+    template_name = 'tracker_app/issue_create.html'
+    form_class = IssueForm
+    success_url = reverse_lazy('issue_list')
 
-    def post(self, request):
-        form = IssueForm(request.POST)
-        if form.is_valid():
-            issue = form.save()
-            return redirect('issue_detail', pk=issue.pk)
-        return render(request, 'tracker_app/issue_create.html', {'form': form})
+    def form_valid(self, form):
+        types = form.cleaned_data.pop('issue_type')
+        issue = form.save()
+        issue.issue_type.set(types)
+        return super().form_valid(form)
 
 
-class IssueUpdateView(View):
-    def get(self, request, pk):
-        issue = get_object_or_404(Issue, pk=pk)
-        form = IssueForm(instance=issue)
-        return render(request, 'tracker_app/issue_update.html', {'form': form, 'issue': issue})
+class IssueUpdateView(FormView):
+    template_name = 'tracker_app/issue_update.html'
+    form_class = IssueForm
 
-    def post(self, request, pk):
-        issue = get_object_or_404(Issue, pk=pk)
-        form = IssueForm(request.POST, instance=issue)
-        if form.is_valid():
-            form.save()
-            return redirect('issue_detail', pk=issue.pk)
-        return render(request, 'tracker_app/issue_update.html', {'form': form, 'issue': issue})
+    def dispatch(self, request, *args, **kwargs):
+        self.issue = self.get_object()
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_object(self):
+        return get_object_or_404(Issue, pk=self.kwargs.get('pk'))
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['issue'] = self.issue
+        return context
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['instance'] = self.issue
+        return kwargs
+
+    def form_valid(self, form):
+        types = form.cleaned_data.pop('issue_type')
+        issue = form.save()
+        issue.issue_type.set(types)
+        return redirect('issue_detail', pk=issue.pk)
 
 
 class IssueDeleteView(View):
-    def get(self, request, pk):
-        issue = get_object_or_404(Issue, pk=pk)
-        return render(request, 'tracker_app/issue_delete.html', {'issue': issue})
-
-    def post(self, request, pk):
-        issue = get_object_or_404(Issue, pk=pk)
+    def post(self, request, *args, **kwargs):
+        issue = get_object_or_404(Issue, pk=self.kwargs.get('pk'))
         issue.delete()
         return redirect('issue_list')
