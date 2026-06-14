40dc24ed1f5e70654b1729eb32f8bc68836c0078
mad547 <97340914+mad547@users.noreply.github.com>
2026-06-14 22:11:59 +0600

    добавил валидаторы в форму

--- source/tracker_app/forms.py
+++ source/tracker_app/forms.py
@@ -1,7 +1,17 @@
-from django.forms import ModelForm, widgets
-from tracker_app.models import Issue
+from cProfile import label
+
+from django.core.exceptions import ValidationError
+from django.forms import ModelForm, widgets, ModelMultipleChoiceField, CheckboxSelectMultiple
+from tracker_app.models import Issue, Type
 
 class IssueForm(ModelForm):
+    issue_type = forms.ModelChoiceField(
+        queryset=Type.objects.all(),
+        required=False,
+        label='Тип',
+        widjet=CheckboxSelectMultiple()
+    )
+
     class Meta:
         model = Issue
         fields = ['summary','description', 'status' ,'issue_type']
@@ -22,3 +32,17 @@ class IssueForm(ModelForm):
                 'class': 'form-control',
             }),
         }
+
+    def clean_summary(self):
+        summary = self.cleaned_data['summary']
+        if len(summary) < 10:
+            raise ValidationError('Краткое описание должно быть не короче 10 символов')
+        return summary
+
+    def clean(self):
+        cleaned_data = super().clean()
+        summary = self.cleaned_data.get('summary')
+        description = self.cleaned_data.get('description')
+        if summary and description and summary == description:
+            raise ValidationError('краткое и полное описание не должны совпадать')
+        return cleaned_data
