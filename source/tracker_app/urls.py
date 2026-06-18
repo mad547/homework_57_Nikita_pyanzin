199560967e6a4933702d9b91e81618c2c5b8168b
mad547 <97340914+mad547@users.noreply.github.com>
2026-06-18 20:54:37 +0600

    обновил urls главная страница теперь список проектов

--- source/tracker_app/urls.py
+++ source/tracker_app/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
 from tracker_app.views import (
-IssueListView, IssueDetailView,
-IssueCreateView, IssueUpdateView, IssueDeleteView
+IssueListView, IssueDetailView, IssueCreateView, IssueUpdateView, IssueDeleteView,
+ProjectListView, ProjectDetailView, ProjectCreateView, ProjectUpdateView, ProjectDeleteView,
+ProjectIssueCreateView,
 )
 
 
 urlpatterns = [
-    path('', IssueListView.as_view(), name='issue_list'),
+    path('', ProjectListView.as_view(), name='project_list'),
+    path('projects/create/', ProjectCreateView.as_view(), name='project_create'),
+    path('projects/<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
+    path('projects/<int:pk>/update/', ProjectUpdateView.as_view(), name='project_update'),
+    path('projects/<int:pk>/delete/', ProjectDeleteView.as_view(), name='project_delete'),
+    path('projects/int:project_pk/issues/create/', IssueCreateView.as_view(), name='issue_create'),
+
+    path('issues', IssueListView.as_view(), name='issue_list'),
     path('issues/<int:pk>/', IssueDetailView.as_view(), name='issue_detail'),
     path('issues/create/', IssueCreateView.as_view(), name='issue_create'),
     path('issues/<int:pk>/update/', IssueUpdateView.as_view(), name='issue_update'),
