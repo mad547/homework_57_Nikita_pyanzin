7e5e61e4450d0ea7aa24d3e60d27e0410d01b729
mad547 <97340914+mad547@users.noreply.github.com>
2026-06-18 18:55:46 +0600

    добавил projectform and issueprojectform

--- source/tracker_app/forms.py
+++ source/tracker_app/forms.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import ValidationError
 from django.forms import ModelForm, widgets, ModelMultipleChoiceField, CheckboxSelectMultiple
-from tracker_app.models import Issue, Type
+from tracker_app.models import Issue, Type, Project
 
 class IssueForm(ModelForm):
     issue_type = ModelMultipleChoiceField(
@@ -41,3 +41,61 @@ class IssueForm(ModelForm):
         if summary and description and summary == description:
             raise ValidationError('краткое и полное описание не должны совпадать')
         return cleaned_data
+
+
+class ProjectForm(ModelForm):
+    class Meta:
+        model = Project
+        fields = ['name','description', 'start_date' ,'end_date']
+        widgets = {
+            'name': widgets.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Название проекта'
+            }),
+            'description': widgets.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 4,
+                'placeholder': 'Описание проекта'
+            }),
+            'start_date': widgets.DateInput(attrs={
+                'class': 'form-control',
+                'type': 'date',
+            }),
+            'end_date': widgets.DateInput(attrs={
+                'class': 'form-control',
+                'type': 'date',
+            })
+        }
+
+
+class IssueInProjectForm(ModelForm):
+    issue_type = ModelMultipleChoiceField(
+        queryset=Type.objects.all(),
+        required=False,
+        label='Тип',
+        widget=CheckboxSelectMultiple()
+    )
+
+    class Meta:
+        model = Issue
+        fields = ['summary','description', 'status' ,'issue_type']
+        widgets = {
+            'summary': widgets.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Краткое описание'
+            }),
+            'description': widgets.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 4,
+                'placeholder': 'Полное описание'
+            }),
+            'status': widgets.Select(attrs={
+                'class': 'form-control',
+            }),
+        }
+
+    def clean_summary(self):
+        summary = self.cleaned_data['summary']
+        if len(summary) < 10:
+            raise  ValidationError('Краткое описание должно быть не короче 10 символов')
+        return summary
