ed7ff74ec5c22154aded251086245856d1dc1582
mad547 <97340914+mad547@users.noreply.github.com>
2026-06-18 18:12:56 +0600

    зарегестрировал project в админке

--- source/tracker_app/admin.py
+++ source/tracker_app/admin.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
-from tracker_app.models import Type, Status, Issue
+from tracker_app.models import Type, Status, Issue, Project
 
 
 # Register your models here.
 class IssueAdmin(admin.ModelAdmin):
-    list_display = ['id', 'summary', 'status', 'created_at',]
-    list_filter = ['status',]
+    list_display = ['id', 'summary', 'status', 'project', 'created_at',]
+    list_filter = ['status', 'project']
     search_fields = ['summary']
 
 
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'start_date', 'end_date',]
+    search_fields = ['name', 'description']
+
 admin.site.register(Type)
 admin.site.register(Status)
+admin.site.register(Project, ProjectAdmin)
 admin.site.register(Issue, IssueAdmin)
