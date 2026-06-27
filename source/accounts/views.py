cd638425f91f22cd96a5e9a532746d5b9b879334
mad547 <97340914+mad547@users.noreply.github.com>
2026-06-28 05:47:22 +0600

    добавил registerviev для регистрации

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from accounts.forms import MyUserCreationForm
+
+
+class RegisterView(CreateView):
+    model = User
+    template_name = 'accounts/register.html'
+    form_class = MyUserCreationForm
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.get_success_url())
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if not next_url:
+            next_url = self.request.POST.get('next')
+        if not next_url:
+            next_url = reverse_lazy('project_list')
+        return next_url
 
 # Create your views here.
